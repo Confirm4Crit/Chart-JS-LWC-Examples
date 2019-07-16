5d5f3278d1e1e304a02294bbee604072ae3f8824
Miles Robson <miles_robson@yahoo.com>
2019-07-15 22:11:19 -0400

    checkpoint commit

---
+++ force-app/main/default/lwc/chartJsPieExample/chartJsPieExample.js
@@ -0,0 +1,7 @@
+/**
+ * Created by miles on 6/23/2019.
+ */
+
+import { LightningElement } from 'lwc';
+
+export default class chartJsPieExample extends LightningElement {}
--- force-app/main/default/lwc/libsChartjs/libsChartjs.js
+++ force-app/main/default/lwc/libsChartjs/libsChartjs.js
@@ -1,6 +1,7 @@
-import { LightningElement, track } from 'lwc';
+import { api, LightningElement, track } from 'lwc';
 import { loadScript } from 'lightning/platformResourceLoader';
 import chartjs from '@salesforce/resourceUrl/chart';
+import myNewFunction from '@salesforce/apex/ChartJsSoqlRetriever.myNewFunction';
 
 const generateRandomNumber = () => Math.round(Math.random() * 100);
 
@@ -8,48 +9,67 @@ export default class LibsChartjs extends LightningElement {
   @track error;
   chart;
   chartjsInitialized = false;
+  @api soqlquery;
+  @api label;
+  @api value;
 
-  config = {
-    type: 'doughnut',
-    data: {
-      datasets: [
-        {
-          data: [5, 5, 5, 5, 5],
-          backgroundColor: [
-            'rgb(255, 99, 132)',
-            'rgb(255, 159, 64)',
-            'rgb(255, 205, 86)',
-            'rgb(75, 192, 192)',
-            'rgb(54, 162, 235)',
-          ],
-          label: 'Dataset 1',
-        },
-      ],
-      labels: ['Red', 'Orange', 'Yellow', 'Green', 'Blue'],
-    },
-    options: {
-      responsive: true,
-      legend: {
-        position: 'right',
-      },
-      animation: {
-        animateScale: true,
-        animateRotate: true,
-      },
-    },
-  };
   renderedCallback() {
-    if (this.chartjsInitialized) {
-      return;
-    }
-    this.chartjsInitialized = true;
-    loadScript(this, chartjs)
-      .then(() => {
-        const ctx = this.template.querySelector('canvas.donut').getContext('2d');
-        this.chart = new window.Chart(ctx, this.config);
+    console.log(this.soqlquery);
+    myNewFunction({ iQuery: this.soqlquery })
+      .then((result) => {
+        console.log(result);
+        const newData = [];
+        const newLabel = [];
+        result.forEach((element) => {
+          newData.push(element[this.value]);
+          newLabel.push(element[this.label]);
+        });
+        const config = {
+          type: 'doughnut',
+          data: {
+            datasets: [
+              {
+                data: newData,
+                backgroundColor: [
+                  'rgb(255, 99, 132)',
+                  'rgb(255, 159, 64)',
+                  'rgb(255, 205, 86)',
+                  'rgb(75, 192, 192)',
+                  'rgb(54, 162, 235)',
+                ],
+                label: 'Dataset 1',
+              },
+            ],
+            labels: newLabel,
+          },
+          options: {
+            responsive: true,
+            legend: {
+              position: 'right',
+            },
+            animation: {
+              animateScale: true,
+              animateRotate: true,
+            },
+          },
+        };
+        console.log(config);
+        if (this.chartjsInitialized) {
+          return;
+        }
+        this.chartjsInitialized = true;
+        loadScript(this, chartjs)
+          .then(() => {
+            const ctx = this.template.querySelector('canvas.donut').getContext('2d');
+            this.chart = new window.Chart(ctx, config);
+          })
+          .catch((error) => {
+            this.error = error;
+          });
       })
       .catch((error) => {
-        this.error = error;
+        // TODO Error handling
       });
+    console.log(this.config);
   }
 }
